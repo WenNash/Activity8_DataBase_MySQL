3b28fa864f26b7fb435ca6b5cc517bbfdea87b0b
Wean Nashrullah <weannashrullah@gmail.com>
2021-06-10 23:13:17 +0700

    Nama : Wean Nashrullah NIM : 20190140076 Kelas : B

--- app/src/main/java/com/example/database_mysql/MainActivity.java
+++ app/src/main/java/com/example/database_mysql/MainActivity.java
@@ -79,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 }
+                adapter.notifyDataSetChanged();
             }
         }, new Response.ErrorListener(){
             @Override
